0b60886d2702db4c013b1380ba322d3d5c4850fd
Emin Mammadzada <eminmamedzade37@gmail.com>
2022-11-11 00:02:01 -0500

    hotfix for middleware

--- middleware.js
+++ middleware.js
@@ -1,18 +1,16 @@
 const admin = require('./config/firebase-config');
-class Middleware {
-    async decodeToken(req, res, next) {
-        const token = req.headers.authorization.split(' ')[1];
-        try {
-            const decodeValue = await admin.auth().verifyIdToken(token);
-            if (decodeValue) {
-                console.log(decodeValue);
-                req.user = decodeValue["user_id"]
-                return next();
-            }
-            return res.json({ message: 'Unauthorized' });
-        } catch (e) {
-            return res.json({ message: 'Internal Error' });
+
+module.exports.decodeToken = async (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1];
+    try {
+        const decodeValue = await admin.auth().verifyIdToken(token);
+        if (decodeValue) {
+            console.log(decodeValue);
+            req.user = decodeValue["user_id"]
+            return next();
         }
+        return res.json({ message: 'Unauthorized' });
+    } catch (e) {
+        return res.json({ message: 'Internal Error' });
     }
 }
-module.exports = new Middleware();
