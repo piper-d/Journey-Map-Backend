b07c390da197e7d7ce95496ad84bb5cf9d332a26
Emin Mammadzada <eminmamedzade37@gmail.com>
2023-03-20 14:21:45 -0400

    fix the delete user

--- functions/routes/userRoutes.js
+++ functions/routes/userRoutes.js
@@ -2,10 +2,10 @@
 // Requirements and dependencies
 
 const userRouter = require("express").Router();
-const {decodeToken} = require("../middleware");
+const { decodeToken } = require("../middleware");
 const AppError = require("../utils/AppError");
 const admin = require("../config/firebase-config");
-
+const FirebaseStorage = admin.storage();
 
 // /////////////////////////////////
 // Initialization
@@ -35,8 +35,8 @@ userRouter.put("/user", decodeToken, async (req, res, next) => {
     const user = await db.collection("Users").doc(req.user);
     const snap = await db.collection("Users").doc(req.user).get();
     const data = snap.data();
-    const {username = data.username} = req.body;
-    await user.update({username: username});
+    const { username = data.username } = req.body;
+    await user.update({ username: username });
     const updatedData = await (await db.collection("Users").doc(req.user).get()).data();
     return res.status(200).json(updatedData);
   } catch (e) {
@@ -55,25 +55,25 @@ userRouter.delete("/user", decodeToken, async (req, res, next) => {
         const tripRef = await db.collection("Trips").doc(doc.id);
 
         // remove the media associated with the trip associated with the user from the object store
-        const snap_media = await trip_ref.get();
+        const snap_media = await tripRef.get();
         if (snap_media.exists) {
           const data = snap_media.data();
-          if (data["media"]) {
-            iterableData = data["media"];
-            for (const [coords, urls] of Object.entries(iterableData)) {
-              for (const url of urls) {
-                const fileName = url.split("appspot.com/")[1];
-                const bucketName = "journeymap-a8e65.appspot.com";
-                const file = FirebaseStorage.bucket(bucketName).file(fileName);
-                file.delete()
-                    .then(() => {
-                      console.log("Successfully deleted image from Object Store");
-                    }).catch((err) => {
-                      return next(new AppError(`could not delete image from object storage, ${err}`, 400));
-                    });
-              }
-            }
-          }
+          // if (data["media"]) {
+          //   iterableData = data["media"];
+          //   for (const [coords, urls] of Object.entries(iterableData)) {
+          //     for (const url of urls) {
+          //       const fileName = url.split("appspot.com/")[1];
+          //       const bucketName = "journeymap-a8e65.appspot.com";
+          //       const file = FirebaseStorage.bucket(bucketName).file(fileName);
+          //       file.delete()
+          //         .then(() => {
+          //           console.log("Successfully deleted image from Object Store");
+          //         }).catch((err) => {
+          //           return next(new AppError(`could not delete image from object storage, ${err}`, 400));
+          //         });
+          //     }
+          //   }
+          // }
         }
         // delete the trip from the trips collection
         await tripRef.delete();
@@ -86,7 +86,7 @@ userRouter.delete("/user", decodeToken, async (req, res, next) => {
     // remove the user from authentication of firebase
     await admin.auth().deleteUser(req.user);
 
-    return res.status(200).json({"error": ""});
+    return res.status(200).json({ "error": "" });
   } catch (e) {
     return next(new AppError(`Bad request, ${e}`, 400));
   }
